36be53db098e244db46ead899b3cbea2b1e2661e
Alex <lushinaleksei@bk.ru>
2021-08-19 00:37:47 +0400

    Работает собственный датчик

--- main/java/com/example/demo/MyThread.java
+++ main/java/com/example/demo/MyThread.java
@@ -5,25 +5,30 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Component
 public class MyThread extends Thread {
     private final Counter testCounter;
+    private final AtomicInteger testGauge;
 
     public MyThread(MeterRegistry meterRegistry) {
+        testGauge = meterRegistry.gauge("myGauge", new AtomicInteger(0));
         testCounter = meterRegistry.counter("myCounter");
         this.start();
     }
 
     @Override
     public void run(){
-        while (true) {
+        do {
             testCounter.increment();
-
+            testGauge.set(12938);
             try {
                 sleep(1000L);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+
+        } while (!(testCounter.count() > 1000.0));
     }
 }
